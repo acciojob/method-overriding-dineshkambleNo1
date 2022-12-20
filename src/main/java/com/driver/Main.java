82b46562bca9722211c0505be9ee806260c47cba
dinesh <dineshrock153@gmail.com>
2022-12-20 20:44:03 +0530

    msh

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
-  
+
+    static class A {
+        String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class  B extends A {
+
+        // static method cannot give or take from non-static
+
+        @Override
+        String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String[] args) {
+
+        B b = new B();
+        b.meth();
+
+        B b1 = new B();
+        b1.meth();
+    }
 }
